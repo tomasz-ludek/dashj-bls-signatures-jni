32ebecb70861b70fbb941563819f0b4bae663320
tomasz-ludek <KrowaWKropkiBordo>
2018-11-06 10:12:09 -0500

    Configuring NetBeans (native) and IntelliJ (java) on Ubuntu 16.04

---
+++ CMakeFiles/dashjbls.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dashjbls.dir/src/org_dash_bls_DashBlsJni.cpp.o"
+  "lib/libdashjbls.pdb"
+  "lib/libdashjbls.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dashjbls.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
