57a5e9fc071314f8eda0ab12a41d59b22c119e59
tomasz-ludek <KrowaWKropkiBordo>
2018-11-06 12:57:05 -0500

    Code refactoring

--- src/java/org/dash/bls/DashBlsJni.java
+++ src/java/org/dash/bls/DashBlsJni.java
@@ -7,27 +7,25 @@ public class DashBlsJni {
     }
      
     public static void main(String[] args) {
-        
+
+        DashBlsJni blsJni = new DashBlsJni();
+        blsJni.nativeSignMessage();
+
         int[] seed = new int[] {
             0, 50, 6, 244, 24, 199, 1, 25, 52, 88, 192,
             19, 18, 12, 89, 6, 220, 18, 102, 58, 209,
             82, 12, 62, 89, 110, 182, 9, 44, 20, 254, 22};
 
-        DashBlsJni blsJni = new DashBlsJni();
-		blsJni.nativeSignMessage();
-		
-//        long fingerprint = blsJni.publicKeyFingerprint(seed);
-//        System.out.println("Fingerprint (java):\t\t" + fingerprint);
-
         byte[] privateKey = blsJni.generatePrivateKey(seed);
-//        byte[] publicKey = blsJni.generatePublicKey(privateKey);
-//        for (byte item : privateKey) {
-//            System.out.println("item:\t" + (item & 0xFF));
-//        }
 
         int[] msg = new int[] {100, 2, 254, 88, 90, 45, 23};
 
         byte[] sign = blsJni.signMessage(privateKey, msg);
+
+        printSign(sign);
+    }
+
+    private static void printSign(byte[] sign) {
         System.out.print("java sign:  \t{");
         for (byte item : sign) {
             System.out.print((item & 0xFF) + ", ");
